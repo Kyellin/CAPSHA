c1c8d0fdc0a68e016563b6e69830ddaf487afdd5
Kyellin <mhtheriault@hotmail.com>
2017-06-27 09:53:49 -0400

    Add files via upload

---
+++ random.php
@@ -0,0 +1,111 @@
+<?php
+
+/* random de x1*/
+
+$nb_min = 0;
+$nb_max = 75;
+$nombre1 = rand($nb_min,$nb_max);
+
+$nb_min = 0;
+$nb_max = 75;
+$nombre2 = rand($nb_min,$nb_max);
+
+$nb_min = 0;
+$nb_max = 75;
+$nombre3 = rand($nb_min,$nb_max);
+
+$nb_min = 0;
+$nb_max = 75;
+$nombre4 = rand($nb_min,$nb_max);
+
+$nb_min = 0;
+$nb_max = 75;
+$nombre5 = rand($nb_min,$nb_max);
+
+$nb_min = 0;
+$nb_max = 75;
+$nombre6 = rand($nb_min,$nb_max);
+
+/*random de y1*/
+
+
+$nb_min = 0;
+$nb_max = 40;
+$nombre7 = rand($nb_min,$nb_max);
+
+$nb_min = 0;
+$nb_max = 40;
+$nombre8 = rand($nb_min,$nb_max);
+
+$nb_min = 0;
+$nb_max = 40;
+$nombre9 = rand($nb_min,$nb_max);
+
+$nb_min = 0;
+$nb_max = 40;
+$nombre10 = rand($nb_min,$nb_max);
+
+$nb_min = 0;
+$nb_max = 40;
+$nombre11 = rand($nb_min,$nb_max);
+
+$nb_min = 0;
+$nb_max = 40;
+$nombre12 = rand($nb_min,$nb_max);
+
+/*random de x2*/
+
+
+$nb_min = 0;
+$nb_max = 75;
+$nombre13 = rand($nb_min,$nb_max);
+
+$nb_min = 0;
+$nb_max = 75;
+$nombre14 = rand($nb_min,$nb_max);
+
+$nb_min = 0;
+$nb_max = 75;
+$nombre15 = rand($nb_min,$nb_max);
+
+$nb_min = 0;
+$nb_max = 75;
+$nombre16 = rand($nb_min,$nb_max);
+
+$nb_min = 0;
+$nb_max = 75;
+$nombre17 = rand($nb_min,$nb_max);
+
+$nb_min = 0;
+$nb_max = 75;
+$nombre18 = rand($nb_min,$nb_max);
+
+/*random de y2*/
+
+
+$nb_min = 0;
+$nb_max = 40;
+$nombre19 = rand($nb_min,$nb_max);
+
+$nb_min = 0;
+$nb_max = 40;
+$nombre20 = rand($nb_min,$nb_max);
+
+$nb_min = 0;
+$nb_max = 40;
+$nombre21 = rand($nb_min,$nb_max);
+
+$nb_min = 0;
+$nb_max = 40;
+$nombre22 = rand($nb_min,$nb_max);
+
+$nb_min = 0;
+$nb_max = 40;
+$nombre23 = rand($nb_min,$nb_max);
+
+$nb_min = 0;
+$nb_max = 40;
+$nombre24 = rand($nb_min,$nb_max);
+
+
+?>
